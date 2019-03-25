6dfa914e0527bbf8df1a89ff8c45d2123209dca4
BorysNie <borysniedbala@wp.pl>
2019-03-25 21:14:16 +0000

    Finalised changes

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo ""
-echo "Starting up a jenkins contaienr"
-
-docker run --name jenkins --cpuset-cpus="0-2" -d -p 8081:8080 -p 50000:50000 -v /etc/jenkins:/var/jenkins_home wouterds/rpi-jenkins
